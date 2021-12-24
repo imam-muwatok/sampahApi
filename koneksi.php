c4f358bece7410b35b96db5bacce601620aaa61b
Muhammad Imam Muwatok <45243273+imam-muwatok@users.noreply.github.com>
2021-12-24 18:03:09 +0700

    edit database

--- koneksi.php
+++ koneksi.php
@@ -1,18 +1,9 @@
 <?php
-// Membuat variabel, ubah sesuai dengan nama host dan database pada hosting 
 $host	= "localhost";
 $user	= "root";
 $pass	= "";
-$db	= "db_kampus";
+$db	= "";
 
-//Menggunakan objek mysqli untuk membuat koneksi dan menyimpan nya dalam variabel $mysqli	
 $mysqli = new mysqli($host, $user, $pass, $db);
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 
 ?>
